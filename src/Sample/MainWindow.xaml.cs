29cc5824b1813890366d5b70deac4a4c52a49663
Max <max.khomyn@gmail.com>
2018-09-28 14:36:36 +0300

    Entry with code and gif

---
+++ src/Sample/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Sample
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> _items = new List<string>();
+        List<string> ItemsCollection { get => _items; set => _items = value; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeItems();
+        }
+
+        private void InitializeItems()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                ItemsCollection.Add($"Item #{i}");
+            }
+            ListBox.ItemsSource = ItemsCollection;
+        }
+    }
+}
